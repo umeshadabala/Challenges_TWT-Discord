3384f46a543881f5f58cd0d51aa6281d1914f7b6
umeshlab <84436237+umeshlab@users.noreply.github.com>
2021-12-14 12:10:56 +0530

    Update challenge_84.py

--- challenge_84.py
+++ challenge_84.py
@@ -6,11 +6,13 @@
 #Story:
 #In addition to Boe Jiden having difficulty going up stairs, he also has difficulty writing on the keyboard. He keeps accidently typing numbers when there is no need for them.
 #Task: You are given a number T and T testcases follow, for each testcase remove any numbers embedded into words, and print the resulting sentence.
-
+#code -:
 for i in range(int(input())):
   text = input()
   Output = ''.join(t if t not in map(str,range(0,10)) else "" for t in text)
   print(Output)
+  
+ 
 #Input
 3
 h8i how ar5e y0ou
